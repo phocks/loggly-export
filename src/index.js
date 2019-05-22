428a13d834b86104be16c729b393e190c2576f8c
Joshua Byrd <phocks@gmail.com>
2019-05-22 16:05:07 +1000

    Get the data

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const axios = require("axios");
 const fs = require("fs");
 const dayjs = require("dayjs");
 
-const START_TIME = "2019-05-20T19:02:00.000+10:00";
+// const START_TIME = "2019-05-20T19:00:00.000+10:00"; // WHEN ARTICLE WENT LIVE
+const START_TIME = "2019-05-20T19:00:00.000+10:00";
+let initialTime = dayjs(START_TIME);
 
-let fromTime = dayjs(START_TIME);
-let toTime = fromTime.add(1, "minute");
+const timeArray = Array(60)
 
-console.log(fromTime.format(), toTime.format());
-
-const main = async () => {
+// Accepts a dayjs time
+const processMinute = async minuteFrom => {
   try {
     // Send request for search ID
     const searchResponse = await axios.get("http://abcnews.loggly.com/apiv2/search", {
@@ -18,10 +18,11 @@ const main = async () => {
       params: {
         q: "storyLabIncome",
         size: 100,
-        from: fromTime.format(),
-        until: toTime.format()
+        from: minuteFrom.format(),
+        until: minuteFrom.add(1, "minute").format()
       }
     });
+    console.log("Search ID received");
 
     // Store search ID for later request
     const rsid = searchResponse.data.rsid.id;
@@ -33,14 +34,33 @@ const main = async () => {
         rsid: rsid
       }
     });
+    console.log("Search data received");
 
     // Convert to a JSON string
     const output = JSON.stringify(eventsResponse.data);
 
-    fs.writeFileSync("data.json", output);
+    fs.writeFileSync("output/" + minuteFrom.format() + ".json", output);
+    console.log("File written");
+    return true;
   } catch (error) {
     console.error(error);
   }
 };
+
+let nextTime = initialTime;
+
+
+const main = async () => {
+  for (const item of timeArray) {
+    await processMinute(nextTime);
+    await sleep(5000);
+    nextTime = nextTime.add(1, "minute");
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 main();
